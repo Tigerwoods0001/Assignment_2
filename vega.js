c72dd25fb35c8093c2e33023d49c8b349742aa93
Neharika Shimpiger <104868833+Tigerwoods0001@users.noreply.github.com>
2023-10-21 21:06:39 +1100

    Add files via upload

--- vega.js
+++ vega.js
@@ -4,11 +4,13 @@ vegaEmbed("#map1_Chart", vg_1).then(function(result) {
 var vg_2 = "map2.json";
 vegaEmbed("#map2_chart", vg_2).then(function(result) {
  }).catch(console.error);
- var vg_3 = "bar.json";
-vegaEmbed("#piechart", vg_3).then(function(result) {
+var vg_3 = "bar.json";
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#piechart", vg_4).then(function(result) {
  }).catch(console.error);
  var vg_4 = "types_fish_production.json";
-vegaEmbed("#bar2_chart", vg_4).then(function(result) {
-}).catch(console.error);var vg_6 = "comparison.json";
-vegaEmbed("#dot_chart", vg_6).then(function(result) {
+
+var vg_6 = "comparison.json";
+vegaEmbed("#dot_chart", vg_5).then(function(result) {
 }).catch(console.error);
